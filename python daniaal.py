5d72a0c04c03c7b013b73384e7277c881dde3d1f
DaniaalHussain <44294213+DaniaalHussain@users.noreply.github.com>
2018-10-19 13:58:38 +0100

    Add files via upload

---
+++ python daniaal.py
@@ -0,0 +1,11 @@
+#Assignment 3
+print("Hello World")
+#Assignment 4
+print("Hello World")
+print("I am in my ISD class right now")
+#Assignment 5
+name = input("What is your name? ")
+print("Hi there, what is your name?")
+print (name)
+print("Hello")
+print("How are you?")
---
+++ python week 3 adding numbers.py
@@ -0,0 +1,10 @@
+Python 3.3.3 (v3.3.3:c3896275c0f6, Nov 18 2013, 21:18:40) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> num1=input("enter first number:")
+enter first number:5
+>>> num2=input("enter second number:")
+enter second number:12
+>>> sum=float(num1)+float(num2)
+>>> print("the sum of {0} and {1} is {2}".format(num1,num2,sum))
+the sum of 5 and 12 is 17.0
+>>> 
